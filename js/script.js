4fe14318f5ec0c21abdcce350f4f3ac2bb072d38
Hal Evans <hal.evans@newtoneurope.com>
2023-01-06 13:40:17 +0000

    started on js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Welcome to Tic Tac Toe");
+
+function init() {
+    console.log("Welcome to Tic Tac Toe");
+
+    // Setup document elements
+    const newGame = document.querySelector("#new-game");
+    const resetScores = document.querySelector("#reset-scores");
+    
+    // Setup event listeners
+    newGame.addEventListener("click", e => console.log(e))
+    resetScores.addEventListener("click", e => console.log(e))
+
+}
+
+// Call the initialisation function to get everything setup
+init();
