0b07ac3b0a8c5c95e6242dc9d8cfed427da6d738
Hal Evans <hal.evans@newtoneurope.com>
2023-01-06 14:09:02 +0000

    added hover over square in css

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@ function init() {
     console.log("Welcome to Tic Tac Toe");
 
     // Setup document elements
-    const newGame = document.querySelector("#new-game");
-    const resetScores = document.querySelector("#reset-scores");
+    const newGameBtn = document.querySelector("#new-game");
+    const resetScoresBtn = document.querySelector("#reset-scores");
+    const squares = document.querySelectorAll(".square");
+    console.log(squares);
     
     // Setup event listeners
-    newGame.addEventListener("click", e => console.log(e))
-    resetScores.addEventListener("click", e => console.log(e))
-
+    newGameBtn.addEventListener("click", e => console.log(e))
+    resetScoresBtn.addEventListener("click", e => console.log(e))
+    squares.forEach(element => {
+        element.addEventListener("click", e => console.log(e.target));
+    });
 }
 
 // Call the initialisation function to get everything setup
